5cca59bd569cda3343a308a6d96f5b3b484747f0
DinhDuyPham <0366010143duy@gmail.com>
2025-02-27 11:16:15 +0700

    .

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const connectUri = process.env.MONGODB_URI ?? 'mongodb://localhost:27017/portfolio';
+const connectUri = process.env.MONGODB_URI;
+// ?? "m@pdduy.7ucad.mongodb.net/"
 const connect = async () => {
     try {
         await mongoose.connect(connectUri);
